c1fbd0659bd68200846b3fd4ba5090a723167304
LostProgrammer1010 <04_zoster_borscht@icloud.com>
2025-06-17 15:49:15 -0400

    Login Screen finished

--- backend/internal/server/frontend/src/pages/Login.tsx
+++ backend/internal/server/frontend/src/pages/Login.tsx
@@ -1,17 +1,36 @@
 import { useAuth } from "../components/Authentication";
+import input from "../styles/Input.module.css"
+import Button from "../styles/Button.module.css"
+import Form from "../styles/Form.module.css"
+import LoginStyles from "./Login.module.css"
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
 
     const { login } = useAuth()
-    
+    const navigate = useNavigate()
 
-    const handleLogin = () => {
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault()
+    
         login()
+        navigate("/search")
     }
 
     return (
         <>
-        <button onClick={handleLogin}>Login</button>
+        <form onSubmit={handleLogin} className={Form.default} action="">
+            <h1>Login</h1>
+            <div className={LoginStyles.input_container}>
+                <input placeholder=" " type="text" className={`${input.default} ${LoginStyles.input}`}/>
+                <label className={LoginStyles.label}>Username</label>
+            </div>
+            <div className={LoginStyles.input_container}>
+                <input placeholder=" " type="password" className={`${input.default} ${LoginStyles.input}`}/>
+                <label className={LoginStyles.label}>Password</label>
+            </div>
+                <button type="submit" className={`${Button.default} ${LoginStyles.button_login}`}>Login</button>
+        </form>
         </>
 
     )
