af4c5babe01f1c661ce24adc8e87d121d263331f
Dustin Meyer <60855358+LostProgrammer1010@users.noreply.github.com>
2025-06-21 19:20:50 -0400

    #116 Computer Search (working) (#117)

--- backend/internal/server/frontend/src/components/cards/ComputerCard.tsx
+++ backend/internal/server/frontend/src/components/cards/ComputerCard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import type { ComputerCardInfo } from "../../models/Computer";
 import ComputerCardStyles from "../../styles/components/ComputerCard.module.css"
 
-const ComputerCard: React.FC<ComputerCardInfo> = ( { Name = "", OU = "" } ) => {
+const ComputerCard: React.FC<ComputerCardInfo> = ( { Name = "NA", OU = "NA", OperatingSystem = "NA" } ) => {
     
     return (
         <div className={ComputerCardStyles.card}>
             <p>Name: { Name }</p>
+            <p>Operating System: { OperatingSystem }</p>
             <p>OU: { OU }</p>
         </div>
     ) 
