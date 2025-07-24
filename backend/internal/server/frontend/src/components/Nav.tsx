0c8007e1fbdbcff9296df8f54a49eecbfdee4fee
Meyer <dmeyer20@URMC-SH.Rochester.edu>
2025-07-24 14:37:47 -0400

    Animation for Info options working

--- backend/internal/server/frontend/src/components/Nav.tsx
+++ backend/internal/server/frontend/src/components/Nav.tsx
@@ -6,26 +6,14 @@ import NavStyles from "../styles/components/Nav.module.css"
 
 const Nav = () => {
 
-    const [scrolled, setScrolled] = useState(false);
-
-    useEffect(() => {
-        const onScroll = () => {
-            setScrolled(window.scrollY > 1)
-        }
-
-        window.addEventListener('scroll', onScroll);
-        return () => window.removeEventListener('scroll', onScroll)
-    });
-
 
 
 
 
     return (
-        <nav className={!scrolled ? NavStyles.nav : `${NavStyles.nav_top}`}>
+        <nav className={`${NavStyles.nav_top}`}>
           <Link className={NavStyles.a} to="/">Home</Link>
           <Link className={NavStyles.a} to="/search">Search</Link>
-          <Link className={NavStyles.a} to="/SummaryIndex">System Summary Index</Link>
 
         </nav>
     )
--- backend/internal/server/frontend/src/pages/User.tsx
+++ backend/internal/server/frontend/src/pages/User.tsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom"
 import Styles from "../styles/pages/User.module.css"
-import React from "react";
+import React, { useState } from "react";
 
 const User = () => {
     const { Username } = useParams<{ Username: string }>();
+    const [currentContent, setCurrentContent] = useState({ id: "info", move: Styles.moveInfo})
 
     React.useEffect(() => {
         document.body.classList.toggle("no-scroll")
@@ -13,17 +14,29 @@ const User = () => {
         }
     }) 
 
-    const handleContentChange = () => {
-        document.getElementById("info")?.classList.toggle(Styles.move)
-         document.getElementById("info2")?.classList.toggle(Styles.move1)
+    const handleContentChange = (id: string, move: string) => {
+        if (currentContent.id == id){
+            return
+        }
 
+        document.getElementById(currentContent.id)?.classList.toggle(currentContent.move)
+
+        document.getElementById(id)?.classList.toggle(move)
+
+        setCurrentContent({id: id, move: move})
     }
 
     return (
         <div className={Styles.layout}>
-            <h1>User Page</h1>
+            <h1>Dustin Meyer</h1>
             <div className={Styles.userInfo}>
-                <button onClick={handleContentChange}>Test</button>
+                <div className={Styles.navigation}>
+                    <button onClick={() => handleContentChange("info", `${Styles.moveInfo}`)}>About</button>
+                    <button onClick={() => handleContentChange("groups", `${Styles.moveGroups}`)}>Groups</button>
+                    <button onClick={() => handleContentChange("sharedrive", `${Styles.moveShareDrive}`)}>Share Drive</button>
+                    <button onClick={() => handleContentChange("lockout", `${Styles.moveLockout}`)}>Lockout</button>
+                </div>
+
                 <ul id="info" className={Styles.content}>
                     <li>Username: {Username}</li>
                     <li>NedID</li>
@@ -35,26 +48,49 @@ const User = () => {
                     <li>Location</li>
                     <li>Password Set</li>
                 </ul>
-                <ul id="info1" className={`${Styles.content} ${Styles.move1}`}>
-                    <li>Username: {Username}</li>
-                    <li>NedID</li>
-                    <li>Email</li>
-                    <li>Relationship Status</li>
-                    <li>Department</li>
-                    <li>Title</li>
-                    <li>Phone</li>
-                    <li>Location</li>
-                    <li>Password Set</li>
+                <ul id="groups" className={`${Styles.content} ${Styles.moveGroups}`}>
+                    Groups content
                 </ul>
-                <table id="info2" className={`${Styles.content} ${Styles.move1}`}>
+                <ul id="sharedrive" className={`${Styles.content} ${Styles.moveShareDrive}`}>
+                    Sharedrive content
+                </ul>
+                <table id="lockout" className={`${Styles.content} ${Styles.table} ${Styles.moveLockout}`}>
+                    <colgroup>
+                        <col style={{width: "33%"}}/>
+                        <col style={{width: "33%"}}/>
+                        <col style={{width: "33%"}}/>
+                    </colgroup>
                     <thead>
-                        <th>Name</th>
-                        <th>Count</th>
-                        <th>Time</th>
+                        <tr>
+                            <th>Name</th>
+                            <th>Count</th>
+                            <th>Time</th>
+                        </tr>
                     </thead>
+                    <tbody>
+                        <tr>
+                            <td>AD22PDC01</td>
+                            <td>0</td>
+                            <td>07/11/2025 16:59:55</td>
+                        </tr>
+                                                <tr>
+                            <td>AD22PDC01</td>
+                            <td>0</td>
+                            <td>07/11/2025 16:59:55</td>
+                        </tr>
+                                                <tr>
+                            <td>AD22PDC01</td>
+                            <td>0</td>
+                            <td>07/11/2025 16:59:55</td>
+                        </tr>
+                        <tr>
+                            <td>AD22PDC01</td>
+                            <td>0</td>
+                            <td>07/11/2025 16:59:55</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
-            
         </div>
     )
 }
