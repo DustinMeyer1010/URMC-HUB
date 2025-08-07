3d437dff7e34dd637c6b387df49cf7006cd0e786
Meyer <04_zoster_borscht@icloud.com>
2025-08-07 17:27:02 -0400

    Fix react not loading due to file path

--- backend/internal/server/frontend/src/models/ShareDrive.ts
+++
@@ -1,7 +0,0 @@
-export type ShareDriveComponent = {
-    Server: string,
-    Name: string,
-    Groups: string[],
-    Select: (Type: string, Name: string) => void,
-    ItemsSelected: string[],
-}
--- backend/internal/server/frontend/src/utils/Copy.ts
+++
@@ -1,9 +0,0 @@
-export const Copy = (text: string) => {
-    navigator.clipboard.writeText(text)
-    .then(() => {
-
-    })
-    .catch((err) => {
-        alert(err)
-    })
-}   
